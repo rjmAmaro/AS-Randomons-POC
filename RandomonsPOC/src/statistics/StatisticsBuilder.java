3d8737538b16b3de8fc09d7974bf60a47bc0ee8b
Ricardo Amaro <ricardoamaro@live.com.pt>
2016-06-06 18:09:18 +0100

    StatisticBuilder recebe da Queue

---
+++ RandomonsPOC/src/statistics/StatisticsBuilder.java
@@ -0,0 +1,44 @@
+package statistics;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+public class StatisticsBuilder {
+	private final static String QUEUE_NAME = "games_to_statistics";
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("172.17.0.2");
+		factory.setPort(5672);
+		factory.setUsername("admin");
+		factory.setPassword("rabbito_coco");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+					throws IOException {
+				String message = new String(body, "UTF-8");
+				System.out.println(" [x] Received '" + message + "'");
+
+			}
+		};
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+	}
+
+	private static void work(long gameId) {
+
+	}
+}
